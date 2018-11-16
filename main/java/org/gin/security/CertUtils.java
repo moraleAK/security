2f6b9d3bc0abc259e87fba15a1adf3148cd758c5
Gin <838288125@qq.com>
2018-11-16 23:56:23 +0800

    【update】增加从文件或者bytes读取公钥

--- main/java/org/gin/security/CertUtils.java
+++ main/java/org/gin/security/CertUtils.java
@@ -1,6 +1,6 @@
 package org.gin.security;
 
-import java.io.InputStream;
+import java.io.*;
 import java.security.PublicKey;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateFactory;
@@ -10,6 +10,16 @@ import java.security.cert.CertificateFactory;
  * @since 2018/11/7 20:59
  */
 public class CertUtils {
+    private static String X509 = "X509";
+
+    public static PublicKey getPublicKey(String path) throws Exception {
+        return getPublicKey(new FileInputStream(new File(path)));
+    }
+
+    public static PublicKey getPublicKey(byte[] bytes) throws Exception {
+        return getPublicKey(new ByteArrayInputStream(bytes));
+    }
+
     private static PublicKey getPublicKey(InputStream in) throws Exception {
         CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
         Certificate certificate = certificateFactory.generateCertificate(in);
